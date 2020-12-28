dec7ce781ec7df977670daf6b07722114c2f2a3a
star kiwmi <40295114+6gk@users.noreply.github.com>
2020-12-28 18:01:06 +1000

    star kiwmi

--- config.h
+++ config.h
@@ -2,24 +2,25 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                     /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                   /* -c option; centers dmenu on screen */
+static int centered = 0;                   /* -c option; centers dmenu on screen */
 static int min_width = 500;                /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
+static const char *fonts[] = { "Ubuntu Mono:pixelsize=12:style=bold"};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNorm] = { "#9294a8", "#151823" },
+	[SchemeSel]  = { "#d6d8eb", "#1e212e" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
+	[SchemeSelHighlight]  = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 6;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 16;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
