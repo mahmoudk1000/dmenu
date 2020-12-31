d6d7dc9383159ee7ff8fbb6752c6fcc60f03d2a5
star kiwmi <40295114+6gk@users.noreply.github.com>
2020-12-31 20:45:09 +1000

    [star kiwmi] change comments a bit

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
+/* These settings can be overriden by command line. */
 
 static int topbar = 1;                     /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                   /* -c option; centers dmenu on screen */
@@ -9,7 +9,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 static const char *fonts[] = { "Ubuntu Mono:pixelsize=14.4:style=regular", "Blobmoji:pixelsize=14.4" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*                  fg         bg     */
 	[SchemeNorm] = { "#767676", "#ffffff" },
 	[SchemeSel]  = { "#444444", "#f2f2f2" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
