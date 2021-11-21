cfe1453336b7b2cf821de065b6d00ba79c998839
mahmoudk1000 <mahmoudk1000@gmail.com>
2021-11-21 12:32:42 +0200

    config.h: update default colors

--- config.h
+++ config.h
@@ -6,18 +6,18 @@ static int centered = 1;                   /* -c option; centers dmenu on screen
 static int min_width = 500;                /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "Ubuntu Mono:pixelsize=13.2:style=bold" };
+static const char *fonts[] = { "Iosevka Curly Slab:pixelsize=12.2:style=bold" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                  fg         bg     */
-	[SchemeNorm] = { "#83878C", "#ffffff" },
-	[SchemeSel]  = { "#5B5F66", "#f2f2f2" },
+	[SchemeNorm] = { "#fdf9f7", "#121213" },
+	[SchemeSel]  = { "#121213", "#9dacde" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
-	[SchemeSelHighlight]  = { "#5B5F66", "#f2f2f2" },
-	[SchemeNormHighlight] = { "#5B5F66", "#ffffff" },
+	[SchemeSelHighlight]  = { "#fdf9f7", "#9dacde" },
+	[SchemeNormHighlight] = { "#da9dac", "#121213" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 6;
+static unsigned int lines      = 10;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 36;
 static unsigned int min_lineheight = 8;
