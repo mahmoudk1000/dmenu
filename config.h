0ebc8d8cb9b11e0acb19f4d217e18165929e4fb7
mahmoudk1000 <mahmoudk1000@gmail.com>
2022-06-07 08:56:31 +0200

    look: updated font and colors

--- config.h
+++ config.h
@@ -3,25 +3,25 @@
 
 static int topbar = 1;                     /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                   /* -c option; centers dmenu on screen */
-static int min_width = 500;                /* minimum width when centered */
+static int min_width = 640;                /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "Iosevka Curly Slab:pixelsize=12.2:style=bold" };
+static const char *fonts[] = { "Iosevka:pixelsize=12.2:style=regular" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                  fg         bg     */
-	[SchemeNorm] = { "#fdf9f7", "#121213" },
-	[SchemeSel]  = { "#121213", "#9dacde" },
+	[SchemeNorm] = { "#DCDDDE", "#11171D" },
+	[SchemeSel]  = { "#11171D", "#D7C1ED" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
-	[SchemeSelHighlight]  = { "#fdf9f7", "#9dacde" },
-	[SchemeNormHighlight] = { "#da9dac", "#121213" },
+	[SchemeSelHighlight]  = { "#DCDDDE", "#D7C1ED" },
+	[SchemeNormHighlight] = { "#F28FAD", "#11171D" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 36;
 static unsigned int min_lineheight = 8;
-static unsigned int columns    = 2;
+static unsigned int columns    = 3;
 
 /*
  * Characters not considered part of a word while deleting words
